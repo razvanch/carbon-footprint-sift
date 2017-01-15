9f4a7c9e83e576ceead2acd45cda568c719ea8c6
Razvan Chitu <razvan.ch95@gmail.com>
2017-01-15 23:26:46 +0200

    Add lufthansa mapper

---
+++ server/lufthansaMapper.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const SUBJECT_ROUTE_REGEX = /.*?([A-Z]{3})-([A-Z]{3}).*/i;
+
+module.exports = function (got) {
+  const inData = got['in'];
+
+  let results = [];
+
+  inData.data.map(function (datum) {
+    const jmapInfo = JSON.parse(datum.value);
+
+    var routeGroup = SUBJECT_ROUTE_REGEX.exec(jmapInfo.subject);
+
+    if (routeGroup && routeGroup.length === 3) {
+      results.push({ name: 'routes',
+                     key: jmapInfo.id,
+                     value: routeGroup[1] + '/' + routeGroup[2] });
+    }
+  });
+
+  return results;
+};
