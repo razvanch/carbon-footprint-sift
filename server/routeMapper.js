e1e8d20a62fbad2b1a4b4a720dd640fd192b7419
Razvan Chitu <razvan.ch95@gmail.com>
2017-01-15 23:26:18 +0200

    routeMapper.js: store flight history

--- server/routeMapper.js
+++ server/routeMapper.js
@@ -2,19 +2,37 @@
 
 
 const footprints = require('./footprints.json');
+const airports = require('./airports.json');
 
 module.exports = function (got) {
   const inData = got['in'];
 
-  let totalFootprints = inData.data
-    .map(function (datum) {
-      const route = datum.value.toString().split('/');
-      const source = route[0];
-      const destination = route[1];
-      
-      return footprints[source][destination] || 0;
-    })
-    .reduce((total, footprint) => total + footprint, 0);
+  var totalFootprints = 0;
+  var history = [];
 
-  return [{ name: 'footprint', key: 'FOOTPRINT', value: totalFootprints }];
+  inData.data.forEach((datum) => {
+    const route = datum.value.toString().split('/');
+    const source = route[0];
+    const destination = route[1];
+    const footprint = footprints[source][destination] || 0;
+
+    totalFootprints += footprint;
+
+    history.push({
+      'source': {
+        'code': source,
+        'longitude': airports[source].longitude,
+        'latitude': airports[source].latitude
+      },
+      'destination': {
+        'code': destination,
+        'longitude': airports[destination].longitude,
+        'latitude': airports[destination].latitude
+      },
+      'footprint': footprint
+    });
+  });
+
+  return [{ name: 'footprint', key: 'FOOTPRINT', value: totalFootprints },
+          { name: 'history', key: 'HISTORY', value: history }];
 };
